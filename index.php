a6b2c8e0563f23a3a137418583c2b009f720c397
Piffek <patrykpiwko123412@gmail.com>
2018-02-28 19:01:01 +0100

    ref

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php
 
-//output array(2, 4 ,6, 2);
+//-----------------------output array(2, 4 ,6, 2)-----------------------------------------
 $input = array(2, 3, 4, 6, 2);
 
 $output = array_filter($input, function($elementFromArray){
 	return ($elementFromArray % 2) === 0;
 });
 
+print_r($output);
+//-----------------------------------------------------------------------------------------
+
+
+//-----------------------output array(2, 4)-----------------------------------------------
+function showHeavy($amount){
+    return function($element) use ($amount){
+        return $element > $amount;
+    };
+}
+
+$input = array(2, 3, 4, 1, 2);
+
+$output = array_filter($input, showHeavy(2));
+
+print_r($output);
+//-----------------------------------------------------------------------------------------
